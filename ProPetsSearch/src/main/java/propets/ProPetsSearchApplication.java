89b9a8aa877e8cef366bf1ad12bc98c19816cb8e
SyMS <SyMS@10.0.0.19>
2020-02-22 13:09:29 +0200

    first commit

---
+++ ProPetsSearch/src/main/java/propets/ProPetsSearchApplication.java
@@ -0,0 +1,13 @@
+package propets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPetsSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsSearchApplication.class, args);
+	}
+
+}
